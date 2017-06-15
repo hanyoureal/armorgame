a66e55fd6fa11ea53592b4ef7908bbad6b6b785e
Radu Nedisan <eneddyn3m@yahoo.com>
2017-06-16 00:23:51 +0300

    ui

--- src/modules/elements/Bullet.js
+++ src/modules/elements/Bullet.js
@@ -21,13 +21,23 @@ function Armor(props) {
 
   return (
     <g transform={`translate(${X}, ${Y})`}>
-      <ellipse
-        rx={Width/2}
-        ry={Height/2}
+      <line
+        x1="0" y1="0" x2={0} y2={-Height/2}
+        stroke="black"
         transform={Transform}
-        fill="red"
+        strokeWidth={calibre + 2}
       />
-      <line x1="0" y1="0" x2={X2} y2={Y2} stroke="pink" transform={Transform} strokeWidth={calibre} />
+      <line
+        x1="0" y1="0" x2={X2} y2={Y2}
+        stroke="rgba(0, 0, 0, 0.06)"
+        transform={Transform}
+        strokeWidth={calibre}
+      />
+      <ellipse
+        rx={Height/6}
+        ry={Height/6}
+        fill="black"
+      />
     </g>
   );
 };
